bf00cf261dee7fa24efd075d50716189423d194c
Alexander Liman <liman324@yandex.ru>
2019-05-16 11:19:18 +0600

    Create TDA7313.h
    Не тестировалось!

---
+++ TDA7313.h
@@ -0,0 +1,28 @@
+// liman324@yandex.ru rcl-radio.ru
+
+
+#ifndef TDA7313_H
+#define TDA7313_H
+
+#define TDA7313_address 0x44 // адрес 0b1000100
+
+
+#include <Arduino.h>
+class TDA7313
+{
+  public:
+    TDA7313();
+        void setVolume(int vol); //   0...63     
+	void setAttLR(int att_lr); // 0..31       
+	void setAttRR(int att_rr); // 0..31     
+        void setAttLF(int att_lf); // 0..31         
+        void setAttRF(int att_rf); // 0..31 
+        void setSwitch(int input, int loud, int usil); // 0..3 , 1-on 0-off , 0...3 (0...11.25dB)
+        void setBass(int bass); // -7...+7
+        void setTreble(int treble); //  -7...+7
+	
+  private:
+	void writeWire(char a);
+};
+	
+#endif //TDA7313_H
